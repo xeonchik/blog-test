c92bd702456e63fb5e506bb052d4a989e9ed6022
Максим Тюфтин <mtyuftin@sberhealth.ru>
2022-11-03 12:10:35 +0300

    feat: common structure

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 declare(strict_types=1);
 
+use Blog\Application;
+use Laminas\Diactoros\ServerRequestFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$request = ServerRequestFactory::fromGlobals();
+
+$app = new Application();
---
+++ src/Container.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Blog;
+
+class Container implements \Psr\Container\ContainerInterface
+{
+    public function get(string $id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function has(string $id): bool
+    {
+        // TODO: Implement has() method.
+    }
+}
